67c4e52958dd8947e9f98d0f8f8e8edaf066d410
Herman Zuiakov <gzuyakov@gmail.com>
2023-01-01 13:26:53 +0100

    Create home component

---
+++ todo/src/components/screens/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="bg-gray-900 h-screen text-white">
+            Home
+        </div>
+    );
+};
+
+export default Home;
